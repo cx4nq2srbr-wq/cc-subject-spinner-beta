a4157bb58aa0bc6c57bce60348e2daf2ba6af242
cx4nq2srbr-wq <cx4nq2srbr@privaterelay.appleid.com>
2026-04-21 22:30:29 -0600

    version 4.0.13 beta

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cc-spinner-v4.0.12 beta'; // Bumped version to trigger the update!
+const CACHE_NAME = 'cc-spinner-v4.0.13 beta'; // Bumped version to trigger the update!
 const ASSETS = [
   './',
   './index.html',
