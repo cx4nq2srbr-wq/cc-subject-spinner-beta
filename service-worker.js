89b17ae8e1b0823792220b348d8691f09878b8a0
cx4nq2srbr-wq <cx4nq2srbr@privaterelay.appleid.com>
2026-08-30 23:24:28 -0600

    updated information button styling

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cc-spinner-v4.2.0 beta'; // Bumped version to trigger this new update!
+const CACHE_NAME = 'cc-spinner-v4.2.1 beta'; // Bumped version to trigger this new update!
 const ASSETS = [
   './',
   './index.html',
