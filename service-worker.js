be106ffdb9f322ac0e3c97e4455e43adc7383aa9
cx4nq2srbr-wq <cx4nq2srbr@privaterelay.appleid.com>
2026-09-09 22:07:12 -0600

    updated splash screen

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cc-spinner-v4.4.4 beta'; // Bumped version to trigger this new update!
+const CACHE_NAME = 'cc-spinner-v4.4.5 beta'; // Bumped version to trigger this new update!
 const ASSETS = [
   './',
   './index.html',
